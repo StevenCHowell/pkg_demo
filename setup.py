86153a7b5052220009d78ec7556f3420d289c664
Steven C. Howell <9484229+StevenCHowell@users.noreply.github.com>
2019-02-01 13:39:49 -0500

    change user info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='Throw away code to test packaging',
     author='Steven C. Howell',
-    author_email='steven.c.howell@email.com',
+    author_email='me@some_email.com',
     packages=['pkg'],
     zip_safe=False,
 )
